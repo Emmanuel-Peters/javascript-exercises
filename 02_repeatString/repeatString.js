1cb6e1de8ff75fb573aac1dc71d8e6271f669d4e
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-09-27 08:29:28 +0100

    Exercise 02 done, all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(input_word, num_repeats) {
+if (num_repeats <0)
+{
+    return('ERROR');
+}
+return_word = "";
+for (let i = 0; i < num_repeats; i++)
+{
+    return_word += input_word
+}
 
+return(return_word);
 };
 
 // Do not edit below this line
