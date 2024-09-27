6d5126c7c30609e35a4eb310919b5684041814fc
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-09-27 08:37:39 +0100

    Exercise 03 passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(input_word) {
+let length_input = input_word.length;
 
+let output_word = "";
+
+for(let i = length_input; i > 0; i--)
+{
+    output_word += input_word[i-1];
+}
+
+return(output_word);
 };
-
 // Do not edit below this line
 module.exports = reverseString;
