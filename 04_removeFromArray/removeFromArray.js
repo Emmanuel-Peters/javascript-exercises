912890505e8b13a4fffa291e4484475998b456d0
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-09-28 10:33:57 +0100

    Exercise 04 - things to note
    Initially used splice to find and remove indices. However, the
    splice method reduced the length of the array, which was affecting
    the loop variable. Hence it was not getting to the end. I added
    a 'minus i' line IF a value was removed so that I wouldn't skip,
    but it ended in an endless loop. Decided to just create a new
    list that appended items only if they weren't matching the inputs.
    
    There, proper commit done and dusted! Onwards.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,19 @@
-const removeFromArray = function(input_array, index_remove, index_remove_two) {
+const removeFromArray = function(input_array, index_remove, index_remove_two, index_remove_three, index_remove_four) {
 let length = input_array.length // splice will reduce length, so need seperate int  
-    
+    let output_array = [];
     for (let i = 0; i<length;i++)
     {
-        if (input_array[i] == index_remove || input_array[i] == index_remove_two)
+        if (input_array[i] !== index_remove && input_array[i] !== index_remove_two && input_array[i] !== index_remove_three
+            && input_array[i] !== index_remove_four
+        )
         {
-            input_array.splice(i, 1);
+            output_array.push(input_array[i]);
         }
-        console.log(input_array[i]);
+        
     }
     
-    console.log(input_array);
-    return(input_array);
+    console.log(output_array);
+    return(output_array);
 };
 
 // Do not edit below this line
