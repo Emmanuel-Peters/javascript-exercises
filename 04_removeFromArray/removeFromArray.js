cedcf6c3fc9969a71e949fa7680f487f094d2301
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-09-28 10:14:32 +0100

    splice not working as intended yet

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(input_array, index_remove, index_remove_two) {
+let length = input_array.length // splice will reduce length, so need seperate int  
+    
+    for (let i = 0; i<length;i++)
+    {
+        if (input_array[i] == index_remove || input_array[i] == index_remove_two)
+        {
+            input_array.splice(i, 1);
+        }
+        console.log(input_array[i]);
+    }
+    
+    console.log(input_array);
+    return(input_array);
 };
 
 // Do not edit below this line
