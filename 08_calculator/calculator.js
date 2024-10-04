780442caa6b5ae0ecb09d7970d9c1b741c2de277
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-10-04 09:30:48 +0100

    Exercise 8, calculator, completed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(int1, int2) {
+	return(int1+int2);
 };
 
-const subtract = function() {
-	
+const subtract = function(int1, int2) {
+	return(int1-int2);
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+  let sum = 0;
+	for(let i = 0; i < array1.length; i++)
+  {
+    sum += array1[i];
+  }
+  return(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  let product = 1;
+	for(let i = 0; i < array1.length; i++)
+  {
+    product *= array1[i];
+  }
+  return(product);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return(base ** exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	product = 1
+  for (let i = input; i>0; i--)
+  {
+    product *= i
+  }
+  return(product);
 };
 
 // Do not edit below this line
