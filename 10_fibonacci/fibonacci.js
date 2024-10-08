f681b213e2c98506585f4b4e77425e7f23613150
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-10-08 21:56:01 +0100

    Cheated a little bit by hard coding inputs 0, 1 and 2... still happy

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+// Plan = create Fib sdequence up to the place needed.
+
+if(place == 1 || place == 2)
+{
+    return(1);
+}
+else if (place == 0)
+{
+    return 0;
+}
+else if (place < 0)
+{
+    return('OOPS');
+}
+let fib = [1,1];
+for (let i = 2; i <= place; i++)
+{
+    fib.push(fib[i-2]+fib[i-1]);
+}
+
+return(fib[place-1]);
 
 };
 
