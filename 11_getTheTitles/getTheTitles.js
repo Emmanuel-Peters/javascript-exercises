949b36f7fde6ecb2b1e2ed234115331024adde3d
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-10-11 07:57:31 +0100

    This was not easy as I thought, object methods...

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+let title_array = [];
+for (let i = 0; i < array.length; i++)
+{
+    title_array.push(array[i].title);
+}
+return(title_array);
 };
 
 // Do not edit below this line
