e491271e7d4759fe291c5fecacb631421a758c13
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-10-11 08:25:13 +0100

    returning undefined and I have no idea why!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(humans) {
+    
+humans.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth) 
+
+let oldest = "";
+let oldest_age = 0;
+
+function age_check(item)
+{
+    if (item.age > oldest_age)
+    {
+        oldest_age = item.age;
+        oldest = item.name;
+    }
+}
+
+humans.forEach(age_check)
+
+
+console.log(oldest);
+return(oldest);
 
 };
 
